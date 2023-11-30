a42df740ca6115c63095b5629924db825cc806c8
imminho2212 <130457497+imminho2212@users.noreply.github.com>
2023-11-30 16:24:46 +0900

    Update yolouse.py

--- yolouse.py
+++ yolouse.py
@@ -13,22 +13,23 @@ while True:
 
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = model(frame)
+    confidences = results.xyxy[0][:, 4]
+    class_label = results.xyxy[0][:, 5]
+    threshold = 0.6
+    high_confidence_indices = confidences > threshold
+    boxes = results.xyxy[0][high_confidence_indices].cpu().numpy()
+    objectnumber = int(len(results.xyxy[0][high_confidence_indices]))
+    color = (0, 255, 0) 
+    thickness = 2
 
-    boxes = results.xyxy[0].cpu().numpy()
-    confidences = boxes[:, 4]
-    class_labels = boxes[:, 5]
-
-    threshold = 0.5
-    filtered_boxes = boxes[confidences > threshold]
-
-    for box in filtered_boxes:
+    for box in boxes:
         start_x, start_y, end_x, end_y, confidence, class_label = box
-        color = (0, 255, 0)  #color
-        thickness = 2
         cv2.rectangle(frame, (int(start_x), int(start_y)), (int(end_x), int(end_y)), color, thickness)
         label = f'{model.names[int(class_label)]}: {confidence:.2f}'
         cv2.putText(frame, label, (int(start_x), int(start_y) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, thickness)
 
+    objectnumber = f'count : {objectnumber}'
+    cv2.putText(frame, objectnumber, (20,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, thickness)
     frame1 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     cv2.imshow('',frame1)
 
