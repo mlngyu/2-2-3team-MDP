2b814825f390c5a5321024fdb6d1f6a8a2bdc42a
imminho2212 <130457497+imminho2212@users.noreply.github.com>
2023-11-30 16:59:10 +0900

    Update yolouse.py

--- yolouse.py
+++ yolouse.py
@@ -1,8 +1,17 @@
 import cv2
 import torch
+import mysql.connector
+from mdp_send_db import add_data_to_mariadb #데이터 베이스 업로드
 
 def get_objectnum():
     return objectnumber
+
+
+#데이터 베이스 정보
+db_host = "127.0.0.1"
+db_user = "root"
+db_password = "root"
+db_name = "snow_life"
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='C:/Users/user/Downloads/best.pt', force_reload=True)
 cap = cv2.VideoCapture(0)
@@ -22,6 +31,7 @@ while True:
     high_confidence_indices = confidences > threshold
     boxes = results.xyxy[0][high_confidence_indices].cpu().numpy()
     objectnumber = int(len(results.xyxy[0][high_confidence_indices]))
+    add_data_to_mariadb(db_host, db_user, db_password, db_name, str(objectnumber))
     color = (0, 255, 0) 
     thickness = 2
 
@@ -31,8 +41,8 @@ while True:
         label = f'{model.names[int(class_label)]}: {confidence:.2f}'
         cv2.putText(frame, label, (int(start_x), int(start_y) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, thickness)
 
-    objectnumber = f'count : {objectnumber}'
-    cv2.putText(frame, objectnumber, (20,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, thickness)
+    objectnumber_s = f'count : {objectnumber}'
+    cv2.putText(frame, objectnumber_s, (20,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, thickness)
     frame1 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     cv2.imshow('',frame1)
 
