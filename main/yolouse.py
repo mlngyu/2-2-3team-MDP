206d7d50a2d92dbb947b7173c29869bcd8bbcb50
imminho2212 <130457497+imminho2212@users.noreply.github.com>
2023-12-14 15:47:23 +0900

    Update yolouse.py

--- main/yolouse.py
+++ main/yolouse.py
@@ -1,38 +1,31 @@
 import cv2
 import torch
-import mysql.connector
-from mdp_send_db import add_data_to_mariadb #데이터 베이스 업로드
+import threading
+from mdp_send_db import add_data_to_mariadb
+import queue
 
-def get_objectnum():
-    return objectnumber
+# Define a global variable for object number
+objectnumber = 0
+frame_queue = queue.Queue()
 
-
-#데이터 베이스 정보
-db_host = "127.0.0.1"
-db_user = "root"
-db_password = "root"
-db_name = "snow_life"
-
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='C:/Users/user/Downloads/best.pt', force_reload=True)
-cap = cv2.VideoCapture(0)
-
-while True:
+def get_frame():
     ret, frame = cap.read()
+    return ret,frame
 
-    if not ret or frame is None:
-        print("Error reading frame")
-        continue
+# Function to process frames
+def process_frames():
+    global objectnumber
 
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    ret, frame = get_frame()
     results = model(frame)
     confidences = results.xyxy[0][:, 4]
     class_label = results.xyxy[0][:, 5]
-    threshold = 0.6
+    threshold = 0.7
     high_confidence_indices = confidences > threshold
     boxes = results.xyxy[0][high_confidence_indices].cpu().numpy()
     objectnumber = int(len(results.xyxy[0][high_confidence_indices]))
     add_data_to_mariadb(db_host, db_user, db_password, db_name, str(objectnumber))
-    color = (0, 255, 0) 
+    color = (0, 255, 0)
     thickness = 2
 
     for box in boxes:
@@ -43,11 +36,45 @@ while True:
 
     objectnumber_s = f'count : {objectnumber}'
     cv2.putText(frame, objectnumber_s, (20,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, thickness)
-    frame1 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    cv2.imshow('',frame1)
+    frame_queue.put(frame)
 
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
 
+# Database information
+db_host = "127.0.0.1"
+db_user = "root"
+db_password = "root"
+db_name = "snow_life"
+
+# Load YOLOv5 model
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='C:/Users/user/Downloads/best.pt', force_reload=True)
+# Open video capture
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+# Create a thread for processing frames
+while(1):
+    processing_thread = threading.Thread(target=process_frames)
+
+    # Start the processing thread
+    processing_thread.start()
+
+    # Main thread continues to do other tasks if needed
+    # ...
+    try:
+        frame = frame_queue.get(timeout=1)  # 필요에 따라 타임아웃 값 조정
+        cv2.imshow('', frame)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            print('q')
+            break  # 'q' 키를 누르면 루프를 종료함
+    except queue.Empty:
+        pass
+
+    processing_thread.join()
+
+    # Wait for the processing thread to finish (optional)
+
+# Release resources
 cap.release()
 cv2.destroyAllWindows()
